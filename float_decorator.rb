63a0d0fcb763efaeb91967774998fe3fc4d42c5d
Dario Seminara <darios3@gmail.com>
2012-04-05 20:02:23 -0300

    added fix to prevent call to coerce

--- float_decorator.rb
+++ float_decorator.rb
@@ -4,6 +4,10 @@ class Numeric
   def to_single_precision(decimals)
     FloatPrecisionDecorator.new(self.rounded_mantissa(decimals), decimals)
   end
+
+  def single
+    to_single_precision(23)
+  end
 end
 
 class FloatPrecisionDecorator 
@@ -15,8 +19,16 @@ class FloatPrecisionDecorator
   def method_missing(m,*x)
     # todas las operaciones sobre el numero se ejecutan sobre el float verdadero
     # y se obtiene el verdadero resultado con precision completa del Float original
-    verdadero_resultado = @inner.send(m,*x)
 
+    if x.size > 0
+      if FloatPrecisionDecorator === x.first
+        verdadero_resultado = @inner.send(m,x.first.instance_variable_get(:@inner))
+      else
+        verdadero_resultado = @inner.send(m,*x)
+      end    
+    else
+      verdadero_resultado = @inner.send(m,*x)
+    end
     # si es numeric, truncar y wrappear
     if Numeric === verdadero_resultado
       # se reduce la precision, multiplicando por el factor, redondeando y volviendo a dividir
