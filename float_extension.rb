54b2eef0484890c7152ba03c88e6d17313defde8
Dario Seminara <darios3@gmail.com>
2012-04-05 11:34:53 -0300

    implemented exponent and mantissa

--- float_extension.rb
+++ float_extension.rb
@@ -4,4 +4,37 @@ class Float
     factor = 2**decimals
     (self * factor).round(0).to_f / factor
   end
+
+  # extrae la mantisa del numero
+  def mantissa
+    compute_mantissa_and_exponent unless @mantissa
+    @mantissa
+  end
+
+  def exponent
+    compute_mantissa_and_exponent unless @exponente
+    @exponent
+  end
+
+private
+
+  def compute_mantissa_and_exponent
+
+    x = self
+    exponente = 0
+
+    while (x < 1.0 or x >= 2.0)
+      if x < 1.0
+        exponente = exponente - 1
+        x = x * 2 
+      elsif x >= 2.0
+        exponente = exponente + 1
+        x = x / 2
+      end
+    end
+
+    @exponent = exponente
+    @mantissa = x
+  end
+
 end
--- test_float_extension.rb
+++ test_float_extension.rb
@@ -9,3 +9,15 @@ suite( _set(1.5, 1.52, 1.59, 1.745) => 1.5,
        _set(20.24, 20.0) => 20.0
        ).test( lambda{|x| x.binary_round(1) } )
 
+# extraccion de mantisa binaria
+suite( _set(1.0, 1.25, 1.5, 1.75) => lambda{|x|x}, 
+       _set(2.0, 2.5, 3.0, 3.5) => lambda{|x|x/2},
+       _set(0.5, 0.625, 0.75, 0.875) => lambda{|x|x*2}
+     ).test( :mantissa.to_proc )
+
+# extraccion de exponente binario
+suite( _set(1.0, 1.25, 1.5, 1.75) => 0, 
+       _set(2.0, 2.5, 3.0, 3.5) => 1,
+       _set(0.5, 0.625, 0.75, 0.875) => -1
+     ).test( :exponent.to_proc )
+
