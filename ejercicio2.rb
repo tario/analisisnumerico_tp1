97f967e91bdc3d20305c6e9787092bdefeb18b47
Dario Seminara <darios3@gmail.com>
2012-04-06 11:48:55 -0300

    refactored series to accept parametric stop criteria

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -2,10 +2,12 @@ require "float_decorator"
 
 # efectua la sumatoria de los terminos de una sucesion hasta
 # que se acerque suficiente a un referencia
-def serie(sucesion, referencia, error)
+# el tercer parametro, es el criterio de parada que debera determinar
+# en funcion de x y de n si se debe parar o no
+def serie(sucesion, stop)
   x = 0.0
   n = 0
-  while (x-referencia).abs > error
+  while not stop[x,n]
     x = x + sucesion[n]
     n = n + 1
   end
@@ -20,13 +22,14 @@ end
 
 # efectua la suma de taylor dada la derivada enesima en el punto x0
 # y la diferencia entre x-x0 hasta que se acerque suficiente a una referencia
-def taylor(derivada_n, diferenciax0, referencia, error)
+# el tercer parametro, es el criterio de parada
+def taylor(derivada_n, diferenciax0, stop)
   # crear los terminos de la sumatoria de taylor en funcion de n
   sucesion = lambda{|n| 
       derivada_n[n] * diferenciax0 ** n / factorial(n) }
 
   # efectuar la sumatoria de la serie
-  serie(sucesion, referencia, error)
+  serie(sucesion, stop)
 end
 
 # la derivada enesima de sin(x) evaluada en pi/2 
@@ -43,7 +46,7 @@ derivada_n_sin = lambda{|n|
 }
 # x0 = pi/2
 print "sin(pi/3): "
-print taylor(derivada_n_sin, - Math::PI/6.0, Math::sin(Math::PI/3.0), 0.01), "\n"
+print taylor(derivada_n_sin, - Math::PI/6.0, lambda{|x,n| (x-Math::sin(Math::PI/3.0)).abs < 0.01}), "\n"
 
 
 # la derivada enesima de cos(x) evaluada en pi/2 
@@ -60,5 +63,5 @@ derivada_n_cos = lambda{|n|
 }
 # x0 = pi/2
 print "cos(pi/3): "
-print taylor(derivada_n_cos, - Math::PI/6.0, Math::cos(Math::PI/3.0), 0.01), "\n"
+print taylor(derivada_n_cos, - Math::PI/6.0,  lambda{|x,n| (x-Math::cos(Math::PI/3.0)).abs < 0.01}), "\n"
 
